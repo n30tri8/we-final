e01f35c4a20df361b0b1ac5f8ad9fe9cac87e749
p-torabi-var <pooyatorabi@aut.ac.ir>
2019-06-28 02:54:26 +0430

    implemented all GET routes

--- app.js
+++ app.js
@@ -22,11 +22,62 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 //user routes
-app.get('/api/restaurants/:id', function(req, res, next) {
-  console.log(req.params.id);
-  res.send('respond with a resource');
+app.get('/api/restaurants', function(req, res, next) {
+  let qString = req.query;
+  let qObject = {};
+  if('area' in qString){
+    let t = db.get('addresses').find({area: qString.area}).value();
+    if(t){
+      qObject['address'] = t.id;
+    }
+  }
+  let rests = db.get('restaurants').filter(qObject).value();
+  let this_rests = [];
+  if('category' in qString){
+    let qCats = qString.category;
+    if(!(qCats instanceof Array)){
+      qCats = [qCats]
+    }
+    let all_cats = db.get('cats').value();
+    let this_cats = [];
+    all_cats.forEach(function (item) {
+      if(qCats.includes(item.name)){
+        this_cats.push(item.id);
+      }
+    });
+    this_rests = [];
+    rests.forEach(function (item) {
+      if(item.categories.filter(value => this_cats.includes(value)).length > 0){
+        this_rests.push(item);
+      }
+    });
+  }
+  else{
+    this_rests = rests;
+  }
+  res.json(this_rests);
 });
-
+app.get('/api/restaurants/:id', function(req, res, next) {
+  let rest = db.get('restaurants').find({ id: req.params.id }).value();
+  res.json(rest);
+});
+app.get('/api/restaurants/:id/commnets', function(req, res, next) {
+  let commnet_ids = db.get('restaurants').find({ id: req.params.id }).get('comments').value();
+  let commnets = [];
+  commnet_ids.forEach(function (item) {
+    let t = db.get('comments').find({ id: item }).value();
+    commnets.push(t);
+  });
+  commnets.sort(function(a, b){
+    let keyA = new Date(a.created_at),
+        keyB = new Date(b.created_at);
+    // Compare the 2 dates
+    if(keyA < keyB) return -1;
+    if(keyA > keyB) return 1;
+    return 0;
+  });
+  res.json(commnets);
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
