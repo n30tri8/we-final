0ec1f663ff1cf9e5e2a9d20f9e60a48367a7c8ea
p-torabi-var <pooyatorabi@aut.ac.ir>
2019-06-28 12:20:31 +0430

    implemented post routers

--- app.js
+++ app.js
@@ -62,13 +62,13 @@ app.get('/api/restaurants/:id', function(req, res, next) {
   res.json(rest);
 });
 app.get('/api/restaurants/:id/commnets', function(req, res, next) {
-  let commnet_ids = db.get('restaurants').find({ id: req.params.id }).get('comments').value();
-  let commnets = [];
-  commnet_ids.forEach(function (item) {
+  let comment_ids = db.get('restaurants').find({ id: req.params.id }).get('comments').value();
+  let comments = [];
+  comment_ids.forEach(function (item) {
     let t = db.get('comments').find({ id: item }).value();
-    commnets.push(t);
+    comments.push(t);
   });
-  commnets.sort(function(a, b){
+  comments.sort(function(a, b){
     let keyA = new Date(a.created_at),
         keyB = new Date(b.created_at);
     // Compare the 2 dates
@@ -76,7 +76,49 @@ app.get('/api/restaurants/:id/commnets', function(req, res, next) {
     if(keyA > keyB) return 1;
     return 0;
   });
-  res.json(commnets);
+  res.json(comments);
+});
+app.post('/api/restaurants/:id/commnets', function(req, res, next) {
+    // if() TODO body contains fields
+    //TODO test
+    let rest_id = shid.generate().toString();
+    let comment = {
+        id : rest_id,
+        author : req.body.author,
+        quality: req.body.quality,
+        packaging: req.body.packaging,
+        deliveryTime: req.body.deliveryTime,
+        text: req.body.text,
+        created_at : new Date(Date.now()).toLocaleString()
+    };
+    db.get('comments').push(comment).write();
+    db.get('restaurants').find({ id: req.params.id }).get('comments').push(rest_id).write();
+    let total_rating = db.get('restaurants').find({ id: req.params.id }).get('total_rating').value();
+    total_rating += req.body.quality + req.body.packaging + req.body.deliveryTime;
+    db.get('restaurants').find({ id: req.params.id }).set('total_rating', total_rating).write();
+    let no_comments = db.get('restaurants').find({ id: req.params.id }).get('comments').value().length;
+    db.get('restaurants').find({ id: req.params.id }).set('average_rating', total_rating/(no_comments*3)).write();
+    res.json(db.get('restaurants').find({ id: req.params.id }).value());
+});
+app.post('/api/restaurants', function(req, res, next) {
+    // if() TODO body contains fields
+    //TODO test
+    let rest_id = shid.generate().toString();
+    let rest = {
+        id : rest_id,
+        name : req.body.name,
+        logo: req.body.logo,
+        openingTime: req.body.openingTime,
+        closingTime: req.body.closingTime,
+        address: req.body.address,
+        categories: req.body.categories,
+        foods : req.body.foods,
+        comments : [],
+        total_rating : 0,
+        average_rating: 0
+    };
+    db.get('restaurants').push(rest).write();
+    res.json(db.get('restaurants').find({ id: rest_id }).value());
 });
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
